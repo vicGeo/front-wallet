b2fb06b8017ba82905223285135643c48fe83bf7
vicGeo <vicsanig@alumni.upv.es>
2023-07-12 00:57:19 +0200

    handle deposit and withdraw money

--- src/App.tsx
+++ src/App.tsx
@@ -17,32 +17,76 @@ function App() {
     fetchData();
   }, []);
 
-  const contextValues = useMemo(() => ({
-    data, setData
-  }), [data, setData])
+  const contextValues = useMemo(
+    () => ({
+      data,
+      setData,
+    }),
+    [data, setData]
+  );
 
-  const handletest = () => {
-    const newMovements = {
-      id: data.movements.length + 1,
-      amount: 2000,
-      concept: 1,
-      date: new Date(),
-      order: 'prueba',
+  const handleIngresar = () => {
+    const newData = {
+      balance: data.balance + 50,
+      movements: [
+        ...data.movements,
+        {
+          id: data.movements.length + 1,
+          amount: 50,
+          concept: 0,
+          date: new Date(),
+          order: 'pedido',
+          prevBalance: data.balance,
+          actualBalance: data.balance + 50,
+        },
+      ],
     };
-    setData((prevData) => ({
-      ...prevData,
-      movements: [...prevData.movements, newMovements],
-    }));
+    setData(newData);
+  };
+  const handleRetirar = () => {
+    const newData = {
+      balance: data.balance - 50,
+      movements: [
+        ...data.movements,
+        {
+          id: data.movements.length + 1,
+          amount: 50,
+          concept: 1,
+          date: new Date(),
+          order: 'pedido',
+          prevBalance: data.balance,
+          actualBalance: data.balance - 50,
+        },
+      ],
+    };
+    setData(newData);
   };
 
   return (
     <WalletContext.Provider value={contextValues}>
-    <main className='grid place-items-center'>
-      {JSON.stringify(data)}
-      <section>
-        <button onClick={handletest}>Prueba</button>
-      </section>
-    </main>
+      <main className='grid place-items-center'>
+        <section className='flex items-center justify-center'>
+          <div className='flex flex-col gap-4'>
+            <p className='flex items-center justify-center text-2xl font-medium'>
+              {data.balance}
+            </p>
+            <div className='flex gap-4'>
+              <button
+                onClick={() => handleIngresar()}
+                className='bg-yellow-400 hover:bg-yellow-500 text-black font-medium py-2 px-4 rounded'
+              >
+                Ingresar fondos
+              </button>
+              <button
+                onClick={() => handleRetirar()}
+                className='bg-blue-400 hover:bg-blue-500 text-white font-medium py-2 px-4 rounded'
+              >
+                Retirar fondos
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
     </WalletContext.Provider>
   );
 }
